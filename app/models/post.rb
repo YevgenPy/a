e451e05b4be8a96b932ec884b099623172752541
YevgenPy <77udaw77@gmail.com>
2022-03-29 14:03:26 +0300

    use i18n

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,4 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: {minimum: 3}
   validates :body, presence: true, length: {minimum: 3}
 
-  def formatted_created_at
-    created_at.strftime('%Y-%m-%d %H:%M:%S')
-  end
 end
